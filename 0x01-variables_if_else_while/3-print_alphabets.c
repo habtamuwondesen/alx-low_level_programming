495365f4a321100ed529a4c878d6f6d47c03b6d8
habtamuwondesen <habtamuwondesen9@gmail.com>
2023-02-20 11:41:13 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main	-
+ * Entry point a program that evaluate random number
+ *
+ * Return:always 0
+ */
+
+int main(void)
+{
+	char lc, uc;
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+
+	}
+	for (uc = 'A'; uc <= 'Z'; uc++)
+	{
+		putchar(uc);
+	}
+	putchar('\n');
+	return (0);
+}
