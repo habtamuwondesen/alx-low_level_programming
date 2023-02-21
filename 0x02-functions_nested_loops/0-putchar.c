feba024e508fc595bdfe07679194003f8d77a44f
habtamuwondesen <habtamuwondesen9@gmail.com>
2023-02-21 14:25:07 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include"main.h"
+#include "main.h"
 /**
- * main - print_putchar
- * is written by habtamu
- * Return:0
- */
+ * main - print _putchar
+ * is written by Yohannes Alelign
+ * Return: 0
+*/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
