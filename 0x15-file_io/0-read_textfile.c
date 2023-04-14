ad8584a418feb2a40138b63974fd4f38b752829d
habtamuwondesen <habtamuwondesen9@gmail.com>
2023-04-14 11:30:55 -0700

    letters is the number of letters it should read and print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- read textfile and print to STDOUT
- * @filename:file is read
- * @letters:length of chars to be read
- * Return : number of letters could be  read/ print or 0 is NULL.
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
-	ssize_t m;
-	ssize_t y;
+	ssize_t w;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	y = read(fd, buf, letters);
-	m = write(STDOUT_FILENO, buf, y);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
 	close(fd);
-	return (m);
-
+	return (w);
 }
+
 
